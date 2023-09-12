80226e618fa8bbc20188ac8d474a5b34847e87fa
alexandre <alex.lerosier@gmail.com>
2023-09-12 10:52:23 +0200

    feat (script.js) : pouvoir utiliser le pomodoro

---
+++ js/script.js
@@ -0,0 +1,85 @@
+/* Constantes ----------------------------------------------------- */
+const BOUTON = document.getElementById("commencer");
+const STATUS = document.getElementById("status");
+const TEMPS_PAUSE = 5;
+const TEMPS_TRAVAIL = 25;
+
+/* Fonctions ----------------------------------------------------- */
+BOUTON.addEventListener("click", pomodoro);
+
+/* Variables globales ----------------------------------------------------- */
+let dmc_setInterval;
+let minutes = 0;
+let secondes = 0;
+let ilTravail = true;
+
+/* Fonctions ----------------------------------------------------- */
+
+/**
+ * Renvoies un nombre avec un 0 devant si il n'a qu'un seule chiffre
+ * @param {*} nombre 
+ * @returns 
+ */
+function formatNombreText(nombre) {
+    let string = nombre.toString();
+    string = (string.length < 2) ? '0' + string : string;
+    return string;
+}
+
+/**
+ * Affiche le temps
+ */
+function afficherTemps() {
+    document.getElementById("label_temps").textContent = `${formatNombreText(minutes)} : ${formatNombreText(secondes)}`;
+}
+
+/**
+ * Décrémente la durée
+ */
+function compteARebour() {
+    if (secondes == 0) {
+        if (minutes == 0) {
+            if (ilTravail) {
+                ilTravail = false;
+                STATUS.textContent = "pause";
+
+                minutes = TEMPS_PAUSE;
+                secondes = 0;
+            } else {
+                ilTravail = true;
+                STATUS.textContent = "travail";
+
+                minutes = TEMPS_TRAVAIL;
+                secondes = 0;
+            }
+            return;
+        }
+        minutes--;
+        secondes = 59;
+
+        return;
+    }
+    secondes--;
+}
+
+/**
+ * Lance le pomodoro
+ */
+function pomodoro() {
+    if (
+        BOUTON.textContent === "commencer"
+        || BOUTON.textContent === "recommencer"
+    ) {
+        BOUTON.textContent = "recommencer";
+        
+        minutes = TEMPS_TRAVAIL;
+
+        clearInterval(dmc_setInterval);
+        dmc_setInterval = setInterval(() => {
+            afficherTemps();
+            compteARebour();
+        }, 1000);
+    } else {
+        console.log("contenue textuel du bouton invalide (fonction pomodoro)");
+    }
+}
