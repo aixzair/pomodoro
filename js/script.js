f13d893e01242cbc7b599d9ecf08a06d53130796
alexandre <alex.lerosier@gmail.com>
2023-09-18 08:59:21 +0200

    feat : local storage

--- js/script.js
+++ js/script.js
@@ -20,8 +20,8 @@ INPUT_TEMPS_PAUSE.addEventListener("mousemove", changerDuree);
 
 
 /* Variables globales ----------------------------------------------------- */
-let temps_travail = 25; // Temps en minutes
-let temps_pause = 5;    // Temps en minutes
+let temps_travail = 25;  // Temps en minutes
+let temps_pause = 0;    // Temps en minutes
 
 let minutes = 0;
 let secondes = 0;
@@ -29,8 +29,16 @@ let ilTravail = true;
 let applicationEnFonctionnement = false;
 
 /* Lancement de l'application ---------------------------------------------- */
-INPUT_TEMPS_TRAVAIL.value = temps_travail;
-INPUT_TEMPS_PAUSE.value = temps_pause;
+try {
+    temps_travail = parseInt(localStorage.getItem("temps_travail")) || INPUT_TEMPS_TRAVAIL.value;
+    temps_pause = parseInt(localStorage.getItem("temps_pause")) || INPUT_TEMPS_PAUSE.value;
+} catch (exception) {
+    console.error(exception);
+}
+
+MINUTES.textContent = formatNombreText(temps_travail);
+document.getElementById("temps_travail").textContent = formatNombreText(temps_travail);
+document.getElementById("temps_pause").textContent = formatNombreText(temps_pause);
 
 /* Fonctions --------------------------------------------------------------- */
 
@@ -42,10 +50,22 @@ function changerDuree() {
         INPUT_TEMPS_TRAVAIL.value = temps_travail;
         INPUT_TEMPS_PAUSE.value = temps_pause;
         return;
+    } else if (
+        temps_travail == INPUT_TEMPS_TRAVAIL.value
+        && temps_pause == INPUT_TEMPS_PAUSE.value
+    ) {
+        return;
     }
 
     temps_travail = INPUT_TEMPS_TRAVAIL.value;
     temps_pause = INPUT_TEMPS_PAUSE.value;
+
+    try {
+        localStorage.setItem("temps_travail", JSON.stringify(temps_travail));
+        localStorage.setItem("temps_pause", JSON.stringify(temps_pause));
+    } catch(exception) {
+        console.error(exception);
+    }
 
     MINUTES.textContent = formatNombreText(temps_travail);
     document.getElementById("temps_travail").textContent = formatNombreText(temps_travail);
